c17632afd90f380941b549c89d40b0b683e88f9a
ahmed-hassan-salem <ahmedhassansalem@outlook.com>
2025-04-04 23:49:05 +0200

    test global ip

--- start.py
+++ start.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
             )
             db.session.add(admin)
             db.session.commit()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port= )
